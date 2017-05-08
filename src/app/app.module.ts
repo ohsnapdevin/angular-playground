af9607f698160bd53c8e61a8674a59ca80bfdd19
Devin Luby <Devin@devinluby.local>
2017-05-08 12:42:42 -0700

    Added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TestingComponentComponent } from './testing-component/testing-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestingComponentComponent
   ],
   imports: [
     BrowserModule,
